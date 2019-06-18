38957b66b208683aa6c5834ac1bb0f1e4a1f35e7
Taha A <tahaygun@gmail.com>
2019-06-18 23:20:01 +0200

    hour added to the playTime for the musics longer than 1 hour

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,20 @@
 //秒转换成 时间格式
 export function formatTime(second) {
   let i = 0;
+  let h = 0;
   let s = parseInt(second);
   if (s > 60) {
     i = parseInt(s / 60);
     s = parseInt(s % 60);
     if (i > 60) {
+      h = parseInt(i / 60);
       i = parseInt(i % 60);
     }
   }
   // 补零
   const zero = v => (v >> 0 < 10 ? `0${v}` : v);
-  return [zero(i), zero(s)].join(":");
+  if (h > 0) return [zero(h), zero(i), zero(s)].join(":");
+  else return [zero(i), zero(s)].join(":");
 }
 
 export function createRandomNum(minNum, maxNum) {
