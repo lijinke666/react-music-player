f46435dd855f02c602fd94593ba0f340aa1c8910
Jinke.Li <a1231236677287@163.com>
2020-04-11 00:53:57 +0800

    feat: support umd and esm module

---
+++ .babelrc.js
@@ -0,0 +1,20 @@
+const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+const outputModule = process.env.OUTPUT_MODULE;
+
+module.exports = {
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "modules": outputModule || false
+      }
+    ],
+    "@babel/preset-react"
+  ],
+  "plugins": [
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-proposal-class-properties",
+    env === "test" && "@babel/plugin-transform-modules-commonjs",
+    env === "development" && "react-hot-loader/babel",
+  ].filter(Boolean)
+}
---
+++ webpack.umd.config.js
@@ -0,0 +1,74 @@
+const fs = require('fs')
+const path = require('path')
+const webpack = require('webpack')
+
+const { version, name, description } = require('./package.json')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    [name]: path.resolve(__dirname, 'src/index'),
+  },
+
+  output: {
+    library: name,
+    libraryTarget: 'umd',
+    umdNamedDefine: true, // 是否将模块名称作为 AMD 输出的命名空间
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].min.js',
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
+  },
+  resolve: {
+    enforceExtension: false,
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+        exclude: '/node_modules/',
+        include: [path.resolve('src')],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: { minimize: false, sourceMap: false }
+          }
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin(` \n ${name} v${version} \n ${description}
+     \n ${fs.readFileSync(path.resolve(__dirname, './LICENCE'))}
+  `),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      __DEBUG__: false,
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+    }),
+  ],
+}
