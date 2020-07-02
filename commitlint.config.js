82bf85fbb3f041fe827a5ac96fd07ee53c8f612f
Jinke.Li <a1231236677287@163.com>
2020-07-02 11:04:59 +0800

    ci: optimize github page release

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'chore',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'deps',
+      ],
+    ],
+  },
+}
