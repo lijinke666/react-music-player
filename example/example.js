05f9a77c7a6c964ad6b5faa53c1eba68c2a99770
Jinke.Li <a1231236677287@163.com>
2020-07-06 21:45:58 +0800

    docs: update

--- example/example.js
+++ example/example.js
@@ -1,27 +1,20 @@
-import { hot } from 'react-hot-loader/root'
+import Switch from 'rc-switch'
 import React from 'react'
+import { hot } from 'react-hot-loader/root'
+import swal from 'sweetalert'
+import { name, repository, version } from '../package.json'
 import ReactJkMusicPlayer from '../src'
 import Locale from '../src/config/locale'
-import swal from 'sweetalert'
-import Switch from 'rc-switch'
-import { createRandomNum } from '../src/utils'
 import PLAY_MODE from '../src/config/playMode'
-import lyric from './lyric'
-import { name, version, repository } from '../package.json'
-
 import '../src/styles/index.less'
+import { createRandomNum } from '../src/utils'
 import './example.less'
+
+
 
 /*eslint-disable no-console*/
 
 const audioList1 = [
-  {
-    name: '高尚',
-    singer: '薛之谦',
-    cover: '//cdn.lijinke.cn/nande.jpg',
-    musicSrc: '//cdn.lijinke.cn/gaoshang.mp3',
-    lyric,
-  },
   {
     name: 'Despacito',
     singer: 'Luis Fonsi',
@@ -30,6 +23,17 @@ const audioList1 = [
     musicSrc: () => {
       return Promise.resolve(
         'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3',
+      )
+    },
+  },
+  {
+    name: 'Dorost Nemisham',
+    singer: 'Sirvan Khosravi',
+    cover:
+      'https://res.cloudinary.com/ehsanahmadi/image/upload/v1573758778/Sirvan-Khosravi-Dorost-Nemisham_glicks.jpg',
+    musicSrc: () => {
+      return Promise.resolve(
+        'https://res.cloudinary.com/ehsanahmadi/video/upload/v1573550770/Sirvan-Khosravi-Dorost-Nemisham-128_kb8urq.mp3',
       )
     },
   },
@@ -56,10 +60,11 @@ const audioList2 = [
     },
   },
   {
-    name: '难得',
-    singer: '安来宁',
-    cover: '//cdn.lijinke.cn/nande.jpg',
-    musicSrc: '//cdn.lijinke.cn/nande.mp3',
+    name: 'Despacito',
+    singer: 'Luis Fonsi',
+    cover:
+      'http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg',
+    musicSrc: 'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3'
   },
 ]
 
